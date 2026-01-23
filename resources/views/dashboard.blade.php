ba0ccac533b54fa7dc8b104cdc2d35885f28f15f
sumkai16 <axceelfelis03@gmail.com>
2026-01-23 18:26:59 +0800

    initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MITCOM Dashboard</title>
+</head>
+<body>
+    <h1>Minglanilla Traffic System</h1>
+    <p>System is running successfully.</p>
+</body>
+</html>
